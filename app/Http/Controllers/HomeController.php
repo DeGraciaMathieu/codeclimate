c1990f855711c06ffd3a5c23a42eb7836f3954aa
DeGraciaMathieu <degracia.mathieu@gmail.com>
2023-01-18 18:44:38 +0100

    Trigger argument-count

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index($a, $b, $c, $d, $e)
+    {
+        //
+    }
+}
