ddd7b801d9aa63a002a427c2f9e2333797f8d0ef
DeGraciaMathieu <degracia.mathieu@gmail.com>
2023-01-22 15:15:58 +0100

    Add trigger

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    if (true) {
+        if (true) {
+            if (true) {
+                if (true) {
+
+                }
+            }
+        }
+    }
 });
